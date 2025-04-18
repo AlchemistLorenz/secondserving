79ad4e057a8b30721fe29d78a5915df806f707cc
natannekip <natashakippur@gmail.com>
2025-04-17 19:44:18 -0600

    Updated app.js to have dashboard route

--- frontend/second-serving/src/App.js
+++ frontend/second-serving/src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginRegister from './pages/LoginRegister';
 import Donate from './pages/Donate';
+import Dashboard from './pages/Dashboard';
 import { motion } from 'framer-motion';
 import './App.css';
 
@@ -75,6 +76,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginRegister />} />
         <Route path="/donate" element={<Donate />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
